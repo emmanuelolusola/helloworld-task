ec98eae50625271616ee4ad04680c78fc8fe2217
OYELEKE EMMANUEL OLUSOLA <emmanuelolusolaoyeleke@gmail.com>
2023-10-19 01:20:19 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <CanvasComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CanvasComponent from "@/components/CanvasComponent";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CanvasComponent,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CanvasComponent.vue
@@ -0,0 +1,69 @@
+<template>
+ <div class="canvas">
+    <div
+      class="box"
+      v-for="(box, index) in boxes"
+      :key="index"
+      @click="toggleBox(index)"
+      :style="{ 'background-color': box.selected ? 'rgba(0, 0, 255, 0.5)' : '' }"
+    ></div>
+ </div>
+</template>
+
+<script>
+export default {
+ data() {
+    return {
+      boxes: Array(1024).fill({ selected: false }),
+    };
+ },
+ methods: {
+    toggleBox(index) {
+      // Deselect all boxes
+      this.boxes = this.boxes.map((box) => ({ ...box, selected: false }));
+
+      // Select the box at the given index
+      this.boxes[index].selected = true;
+    },
+ },
+};
+</script>
+
+<style scoped>
+body {
+ background-color: #121212;
+ color: #f5f5f5;
+}
+.canvas {
+ display: grid;
+ grid-template-columns: repeat(32, 1fr);
+ grid-template-rows: repeat(32, 1fr);
+ width: 3200px;
+ height: 3200px;
+}
+
+.box {
+ position: relative;
+ width: 100px;
+ height: 100px;
+ background-color: #212121;
+ box-sizing: border-box;
+}
+
+.box::before,
+.box::after {
+ content: "";
+ position: absolute;
+ width: 6px;
+ height: 6px;
+ background-color: #f5f5f5;
+ border-radius: 50%;
+}
+
+
+
+
+.box:hover {
+ background-color: #313131;
+}
+</style>
